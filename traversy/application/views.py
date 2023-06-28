60dfca4685cebb3db2ff2055209280b8b94e300e
Raphael <rmc3408@protonmail.com>
2023-06-28 10:00:59 -0400

    traversy - 1:33

--- traversy/application/views.py
+++ traversy/application/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    { 'id': 1, 'name': 'python'},
-    { 'id': 2, 'name': 'node'},
-    { 'id': 3, 'name': 'java'},
-]
+import pprint
+from django.forms.models import model_to_dict
+
+# mockRooms = [
+#     { 'id': 0, 'name': 'python'},
+#     { 'id': 1, 'name': 'node'},
+#     { 'id': 2, 'name': 'java'},
+# ]
+
 
 
 def home(request):
     # return HttpResponse('Home page')
-    return render(request, 'application/home.html', { 'data': rooms })
+    # return render(request, 'home.html', { 'data': mockRooms })
+    
+    rooms = Room.objects.all()
+    
+    context = { 'data': rooms }
+    return render(request, 'home.html', context)
 
 
-def room(request):
-    return render(request, 'application/room.html')
+def room(request, id):
+    # return render(request, 'room.html', { 'data': mockRooms[int(id)] })
+    
+    room = Room.objects.get(id=id)
+    
+    #print(room.__dict__)
+    #pprint.pprint(room.__dict__, indent = 4)
+    #print(model_to_dict(room))
+
+    return render(request, 'room.html', { 'data': room })
 
